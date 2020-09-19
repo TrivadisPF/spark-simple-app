ff84ab2a9a82b7e3d7708f01845fab7e74950708
Guido Schmutz <guido.schmutz@trivadis.com>
2020-09-19 22:03:52 +0200

    Update simple-app.py

--- python/simple-app.py
+++ python/simple-app.py
@@ -1,7 +1,7 @@
 """simple-app.py"""
 from pyspark.sql import SparkSession
 
-logFile = "YOUR_SPARK_HOME/README.md"  # Should be some file on your system
+logFile = "/spark/README.md"  # Should be some file on your system
 spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
 logData = spark.read.text(logFile).cache()
 
